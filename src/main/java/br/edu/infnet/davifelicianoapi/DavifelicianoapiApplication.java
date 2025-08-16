b42c73fd0952bf1d9fbfb4a4f1d0c308fdcbac21
Davi Feliciano <dfeliciano37@gmail.com>
2025-08-16 18:04:30 -0300

    commit inicial

---
+++ src/main/java/br/edu/infnet/davifelicianoapi/DavifelicianoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.davifelicianoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavifelicianoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavifelicianoapiApplication.class, args);
+	}
+
+}
